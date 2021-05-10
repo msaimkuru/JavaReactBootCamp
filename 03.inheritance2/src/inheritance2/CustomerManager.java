18ad5f0c29b3c930717d0b6aa2f559b009fe9e9c
msaimkuru <72169214+msaimkuru@users.noreply.github.com>
2021-05-11 00:53:38 +0300

    Add files via upload

---
+++ 03.inheritance2/src/inheritance2/CustomerManager.java
@@ -0,0 +1,10 @@
+package inheritance2;
+
+public class CustomerManager {
+	public void add(Logger logger) {
+		// codes for adding customer
+		System.out.println("Müşteri eklendi");
+
+		logger.log();
+	}
+}
---
+++ 03.inheritance2/src/inheritance2/Logger.java
@@ -0,0 +1,8 @@
+package inheritance2;
+
+// base class
+public class Logger {
+	public void log() {
+		System.out.println("Ortak konfigürasyon");
+	}
+}
